a8c674626cf53b606f284933644b354adc311e68
fido1hn <confidoekeanya@gmail.com>
2023-09-18 12:21:11 +0100

    Initial

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/plugins");
+    return {
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            output: '_site',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+    //    pathPrefix: "/personal-website/"
+    }
+}
