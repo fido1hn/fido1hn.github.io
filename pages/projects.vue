7c8d853fb89e8e0cbcc2073227fef02917ae3fda
Confidence A.E. <confidoekeanya@gmail.com>
2024-06-20 21:07:58 +0100

    initial projects page for desktop & mobile

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,13 +1,48 @@
 <template>
-  <div class="flex">
-    <h1>Project Showcase</h1>
-    <div class="projects">
-      <div></div>
-      <div></div>
+  <div class="">
+    <h1 class="heading">Projects Showcase</h1>
+    <div
+      class="projects flex flex-col justify-center items-center gap-y-5 min-[870px]:gap-x-20 min-[870px]:flex-row">
+      <div class="img-box">
+        <img src="/images/omnifood-webpage.png" alt="" />
+      </div>
+      <div class="img-box small">
+        <img src="/images/natours-webpage.png" alt="" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style>
+.heading {
+  font-size: 56px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 15px;
+  word-spacing: 0;
+}
+.img-box {
+  width: 350px;
+  height: 400px;
+  overflow: hidden;
+  border-radius: 18px;
+}
+
+@media (max-width: 870px) {
+  .heading {
+    font-size: 48px;
+    word-spacing: 100vw;
+    line-height: 1.2;
+  }
+  .img-box {
+    height: 350px;
+    width: 300px;
+  }
+  .img-box.small {
+    height: 90px;
+    width: 300px;
+  }
+}
+</style>
