e421db7b3f06fa0f6fad1991ab295224a7715818
Confidence A.E. <confidoekeanya@gmail.com>
2024-06-30 05:18:49 +0100

    edit: about copy

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="max-w-3xl">
+  <div class="max-w-3xl -mt-14 tablet:mt-0">
     <h1 class="text-3xl tablet:text-5xl font-bold text-center mb-4 uppercase"
       >About</h1
     >
     <div
       class="text-light-peach px-5 text-center space-y-4 leading-7 text-sm min-[870px]:text-lg">
-      <p>I'm Confidence, web developer living in Lagos Nigeria.</p>
       <p>
-        I have more than 7 years of experience in web development, using
-        Wordpress, Shopify, Python, Django, Vue, React, Node, Express, MongoDB,
-        PostgreSQL and more.
+        Hello, I'm Confidence, a web developer based in Lagos, Nigeria. With
+        over 7 years of experience, I've honed my skills in platforms and
+        technologies, including WordPress, Shopify, Python, Django, Vue, React,
+        Node, Express, MongoDB, PostgreSQL and more.
       </p>
       <p>
-        I'm passionate about learning new technologies and building great
-        products that solve real problems.
+        My passion lies in tackling complex challenges and crafting innovative
+        web solutions that not only solve real-world problems but are also built
+        with scalability and ease of maintenance in mind.
+      </p>
+      <p>
+        I believe that great products not only deliver immediate results but
+        also stand the test of time.
       </p>
     </div>
   </div>
