88bf6982420b07905155a8e36df9205658aa7d08
Confidence A.E. <confidoekeanya@gmail.com>
2024-07-16 17:57:38 +0100

    chore: update

--- pages/about.vue
+++ pages/about.vue
@@ -6,10 +6,10 @@
     <div
       class="text-light-peach px-5 text-center space-y-4 leading-7 text-sm min-[870px]:text-lg">
       <p>
-        Hi, I'm Confidence Ekeanya, a Senior Web Developer based in city of
-        Lagos. With over 7 years of experience in platforms and technologies,
-        including JavaScript, TypeScript, Node, Vue, SQL, MongoDB, LangChain,
-        Docker, Supabase and more.
+        Hi, I'm Confidence Ekeanya, Senior Web Developer based in city of Lagos.
+        With over 7 years of experience in platforms and technologies, including
+        JavaScript, TypeScript, Node, Vue, SQL, MongoDB, LangChain, Docker,
+        Supabase and more.
       </p>
       <p>
         My passion is rooted in tackling complex challenges and crafting
