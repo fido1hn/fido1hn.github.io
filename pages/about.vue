979571135dfb0598fadf77d2577dd85a55785363
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-11 04:47:25 +0100

    update: styling

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="container max-w-5xl px-7 -mt-20">
+  <div class="container max-w-5xl px-7 -mt-10">
     <h1 class="text-xl lg:text-3xl font-bold text-center mb-1 lg:mb-3 uppercase"
       >About</h1
     >
     <div
-      class="font-mono text-primary-100 text-center space-y-3 leading-5 lg:leading-7 text-sm md:text-base lg:text-xl">
+      class="font-mono text-primary-100 text-center space-y-3 leading-5 lg:leading-9 text-sm md:text-base lg:text-xl">
       <p>
         Hi, I'm Confidence Ekeanya, Senior Web Developer from the city of Lagos.
         With over 7 years of experience in platforms and technologies, including
         Vue, Tailwind, GSAP, JavaScript, TypeScript, Node, Express, Supabase,
-        MongoDB, Postgres, LangChain, and more.
+        MongoDB, Postgres, Docker, LangChain, and more.
       </p>
       <p>
         My passion is rooted in tackling complex challenges and crafting
