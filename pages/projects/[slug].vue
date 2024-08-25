1e930a16c9be568a0cc4fa3d11d7e6b1ed8e50dc
Confidence A.E. <confidoekeanya@gmail.com>
2024-08-25 03:08:28 +0100

    single project page title fix

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -58,4 +58,7 @@
 const { slug } = useRoute().params;
 const projects = useProjects();
 const project = computed(() => projects.value.find((p) => p.slug === slug));
+useHead({
+  title: `${project.value.title}`,
+});
 </script>
